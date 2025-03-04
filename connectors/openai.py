d6ddd5dac613f741f26705044803eb8c4f7162ec
herma <49307226+hermaplusplus@users.noreply.github.com>
2025-03-04 23:41:02 +0000

    Adds openai connector constructor

--- connectors/openai.py
+++ connectors/openai.py
@@ -1,2 +1,37 @@
 from . import BaseConnector
 
+from openai import OpenAI
+from typing import Optional, Mapping, Union
+import httpx
+from httpx import Timeout, NotGiven
+
+NOT_GIVEN = NotGiven()
+
+class OpenAIConnector(BaseConnector):
+    __model_name : str
+    __client : OpenAI
+
+    def __init__(
+            self,
+            *,
+            model_name:str,
+            **kwargs):
+
+        self.__model_name__ = model_name
+        
+        if self.__model_name == "undefined":
+            raise ValueError("model_name must be defined")
+
+        self.__client = OpenAI(
+            api_key=kwargs.get("api_key", None),
+            organization=kwargs.get("organization", None),
+            project=kwargs.get("project", None),
+            base_url=kwargs.get("base_url", None),
+            websocket_base_url=kwargs.get("websocket_base_url", None),
+            timeout=kwargs.get("timeout", NOT_GIVEN),
+            max_retries=kwargs.get("max_retries", 2),
+            default_headers=kwargs.get("default_headers", None),
+            default_query=kwargs.get("default_query", None),
+            http_client=kwargs.get("http_client", None),
+            _strict_response_validation=kwargs.get("_strict_response_validation", False)
+        )
