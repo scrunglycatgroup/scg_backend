a62e3d88dfdb5ecc23b69b85eb2cd51661d9b3eb
Woonters <woonters@gmail.com>
2025-04-22 12:40:05 +0100

    Add environment variables to run.py

--- run.py
+++ run.py
@@ -7,8 +7,35 @@ import sys
 from pydantic import BaseModel
 from surrealdb import AsyncSurreal
 from surrealdb import RecordID
+from dotenv import load_dotenv
+import os
 
 from llm_dispatcher import Dispatcher
+
+def help():
+    print("""
+    -h : brings up this menu
+
+    We read these environment variables either from the environment or a file `.env`
+    PY_LOGGER : string for the log level (Debug, info, warning, error, critical)
+    PY_KAFKA_HOST: str ip or localhost address of kafka server
+    PY_KAFKA_PORT: str port of the kafka server
+    PY_KAFKA_TOPICS: str comma separated list of kafka topics to write to 
+
+    PY_SURREAL_HOST: str ip or localhost address of the db
+    PY_SURREAL_PORT: str port of the db
+    PY_SURREAL_USER: str Username for the db
+    PY_SURREAL_PASS: str password for the db
+    PY_SURREAL_NAMESPACE: str namespace
+    PY_SURREAL_DB: str db name
+    PY_SURREAL_TABLE: str table writing to
+    """)
+
+def load_env(env_var:str) -> str:
+    var = os.getenv(env_var)
+    if var is None:
+        raise RuntimeError(f"Missing environment variable {env_var}")
+    return var
 
 def start_logger(log_arg: str):
 
@@ -29,10 +56,19 @@ def start_logger(log_arg: str):
     logging.basicConfig(stream=sys.stderr, level=log_level)
     return
 
-start_logger("DEBUG".upper())
+load_dotenv()
+
+if "-h" in sys.argv:
+    help()
+    sys.exit(0)
+
+log_level = load_env("PY_LOGGER")
+start_logger(log_level.upper())
+
+kafka_topics = load_env("PY_KAFKA_TOPICS").split(",")
+db_table = load_env("PY_SURREAL_TABLE")
 
 app = FastAPI()
-
 
 class GenerateBody(BaseModel):
     input: str
@@ -43,20 +79,31 @@ async def startup_event():
     global db
     global producer
     # Create the topics for kafka 
-    admin = KafkaAdminClient(bootstrap_servers='172.17.0.3:9092')
-    if 'message-topic' not in (admin.list_topics()): 
-        topics = [NewTopic('message-topic',num_partitions=1, replication_factor=1)]
+    kafka_host = load_env("PY_KAFKA_HOST")
+    kafka_port = load_env("PY_KAFKA_PORT")
+    admin = KafkaAdminClient(bootstrap_servers=f'{kafka_host}:{kafka_port}')
+    topics = []
+    for topic in kafka_topics:
+        if topic not in (admin.list_topics()): 
+            topics.append(NewTopic(topic,num_partitions=1, replication_factor=1))
+    if topics != []:
         admin.create_topics(topics)
 
     # create the producer we will send data with 
-    producer = KafkaProducer(bootstrap_servers='172.17.0.3:9092', acks=1, retries=3)
+    producer = KafkaProducer(bootstrap_servers=f'{kafka_host}:{kafka_port}', acks=1, retries=3)
     if not producer.bootstrap_connected:
         logging.error("not connected to KAFKA server")
     
     # connect to the database
-    db = AsyncSurreal("ws://localhost:8008")
-    await db.use("test","test")
-    await db.signin({"username":'root',"password":'root'})
+    db_host = load_env("PY_SURREAL_HOST")
+    db_port = load_env("PY_SURREAL_PORT")
+    db_namespace = load_env("PY_SURREAL_NAMESPACE")
+    db_db = load_env("PY_SURREAL_DB")
+    db_user = load_env("PY_SURREAL_USER")
+    db_pass = load_env("PY_SURREAL_PASS")
+    db = AsyncSurreal(f"ws://{db_host}:{db_port}")
+    await db.use(db_namespace,db_db)
+    await db.signin({"username":db_user,"password":db_pass})
     print("Application Starting")
 
 @app.on_event("shutdown")
@@ -66,10 +113,10 @@ async def shutdown_event():
 
 @app.post("/generate")
 async def basic_generate(message: GenerateBody):
-    request = await db.create("request", {"input": message.input.encode("utf-8")})
+    request = await db.create(db_table, {"input": message.input.encode("utf-8")})
     database_id = request['id'].id
     kafka_message = (database_id + " " + message.input).encode("utf-8")
-    producer.send(topic="message-topic",value=kafka_message)
+    producer.send(topic=kafka_topics[0],value=kafka_message)
     # now we loop over waiting a little bit and checking if 'output' is ever written to, look into subscribe_live
     check_rate = 4
     timeout = 120 / check_rate
